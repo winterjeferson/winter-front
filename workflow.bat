445355bb592275de12276580a8c142b0fbbcaf87
Jeferson <winterjeferson@gmail.com>
2020-01-07 14:46:56 -0300

    update package.json

--- workflow.bat
+++ workflow.bat
@@ -1,11 +1,6 @@
-@RD /S /Q "E:\gulp\task"
-IF not exist E:\gulp\task (mkdir E:\gulp\task)
-COPY E:\development\site\branches\framework\winter-front\gulp\gulpfile.js  E:\gulp\
-COPY E:\development\site\branches\framework\winter-front\gulp\package.json  E:\gulp\
-XCOPY E:\development\site\branches\framework\winter-front\gulp\task  E:\gulp\task 
-CD E:\gulp\
+CD E:\development\site\branches\framework\winter-front\development\gulp
 START /MAX cmd.exe /K gulp
-START /MAX /WAIT "" "E:\gulp"
+
 START /MAX /WAIT "" "E:\development\site\branches\framework\winter-front\"
-START /MAX /WAIT "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --auto-open-devtools-for-tabs "http://localhost/e/development/site/branches/framework/winter-front/public"
+START /MAX /WAIT "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --auto-open-devtools-for-tabs "http://localhost/e/development/site/branches/framework/winter-front/homologation"
 START "" "C:\Users\jeferson.winter\AppData\Local\Programs\Microsoft VS Code\Code.exe"
