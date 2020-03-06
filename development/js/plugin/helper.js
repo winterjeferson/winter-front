828e9097a80521e78212a9384ca2c845dbb10b37
jeferson <jeferson.winter@brsupply.com.br>
2020-03-06 17:51:50 -0300

    seo removed

--- development/js/plugin/helper.js
+++ development/js/plugin/helper.js
@@ -1,17 +1,3 @@
-function buildURL(url) {
-    /*removeIf(production)*/ objDebug.debugMethod(this, objDebug.getMethodName()); /*endRemoveIf(production)*/
-    return url.toString()               // Convert to string
-        .normalize('NFD')               // Change diacritics
-        .replace(/[\u0300-\u036f]/g, '') // Remove illegal characters
-        .replace(/\s+/g, '-')            // Change whitespace to dashes
-        .toLowerCase()                  // Change to lowercase
-        .replace(/&/g, '-and-')          // Replace ampersand
-        .replace(/[^a-z0-9\-]/g, '')     // Remove anything that is not a letter, number or dash
-        .replace(/-+/g, '-')             // Remove duplicate dashes
-        .replace(/^-*/, '')              // Remove starting dashes
-        .replace(/-*$/, '');             // Remove trailing dashes
-}
-
 function getUrlParameter(target) {
     /*removeIf(production)*/ objDebug.debugMethod(this, objDebug.getMethodName(), target); /*endRemoveIf(production)*/
     let url = top.location.search.substring(1);
