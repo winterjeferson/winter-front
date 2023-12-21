3c75158079e3b850b5c26058b00948152b1cad7c
Jeferson  Winter <winterjeferson@gmail.com>
2023-12-20 21:01:27 -0300

    table scroll color

--- src/js/wf-plugin/Table.js
+++ src/js/wf-plugin/Table.js
@@ -2,13 +2,17 @@ export class Table {
     constructor() {
         this.elTable = document.querySelectorAll('.table');
         this.cssResponsive = 'table-responsive';
+        this.colorSeparator = '--';
     }
 
     build() {
         this.elTable.forEach((item) => {
             helper.wrapItem(item, this.cssResponsive);
-
             const elParent = item.parentNode.parentNode.querySelector(`.${this.cssResponsive}`);
+            const color = this.getTableColor(item);
+
+            helper.addClass(elParent, 'scrollbar');
+            helper.addClass(elParent, `scrollbar--${color}`);
             helper.wrapItem(elParent, `wrapper-${this.cssResponsive}`);
         });
     }
@@ -17,5 +21,19 @@ export class Table {
         if (!this.elTable) return;
 
         this.build();
+    }
+
+    getTableColor(target) {
+        const css = target.classList;
+        let color = '';
+
+        css.forEach((item) => {
+            const isColor = item.includes(this.colorSeparator);
+
+            if (isColor) {
+                color = item.split(this.colorSeparator)[1];
+            }
+        });
+        return color;
     }
 }
